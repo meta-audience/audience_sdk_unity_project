8df978db4dba6456d48d04b61ae11e063a1035f2
JonnyTsai <Jonny_Tsai@trendmicro.com>
2022-12-13 16:59:40 +0800

    Add Test framework

---
+++ Assets/audience-unity-sdk/Core/audience-unity-sdk/Tests/TestChatMessage.cs
@@ -0,0 +1,119 @@
+using AudienceSDK;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace AudienceSDK.UnitTest {
+    public class TestChatMessage {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestChatSchemaFields1() {
+            string testChatMessage = @"
+{
+   'chat_id':'8395035a-374b-4272-998e-602c29a8f2af',
+    'author':{
+        'platform':'Youtube',
+        'name':'Ian_Luo',
+        'user_id':'120353655',
+        'is_mod':false,
+        'is_sub':false,
+        'badges':null
+    },
+    'orginal_text':'LUL',
+    'message':[
+        {
+        'type':1,
+        'text':'LUL',
+        'url':'https://static-cdn.jtvnw.net/emoticons/v2/425618/static/light/1.0'
+        }
+    ],
+    'utc_timestamp':1656761183069
+}
+";
+            // Check all fields
+            var chatSchema = JsonConvert.DeserializeObject<ChatData>(testChatMessage);
+            Assert.NotNull(chatSchema);
+            Assert.AreEqual(chatSchema.chat_id, "8395035a-374b-4272-998e-602c29a8f2af");
+            Assert.AreEqual(chatSchema.utc_timestamp, "1656761183069");
+
+            var author = chatSchema.author;
+            Assert.NotNull(author);
+            Assert.AreEqual(author.platform, "Youtube");
+            Assert.AreEqual(author.name, "Ian_Luo");
+            Assert.AreEqual(author.user_id, "120353655");
+            Assert.AreEqual(author.is_mod, false);
+            Assert.AreEqual(author.is_sub, false);
+            Assert.IsNull(author.badges);
+
+            var message = chatSchema.message;
+            Assert.NotNull(message);
+            Assert.AreEqual(message.Count, 1);
+            Assert.AreEqual(message[0].type, (int)ChatMessageType.Emoji_2D);
+            Assert.AreEqual(message[0].text, "LUL");
+            Assert.AreEqual(message[0].url, "https://static-cdn.jtvnw.net/emoticons/v2/425618/static/light/1.0");
+            Assert.IsNull(message[0].asset_list);
+        }
+
+        [Test]
+        public void TestChatSchemaFields2() {
+            string testChatMessage = @"
+{
+    'chat_id':'8395035a-374b-4272-998e-602c29a8f2af',
+    'author':{
+        'platform':'Twitch',
+        'name':'tl_tsai',
+        'user_id':'120353655',
+        'is_mod':false,
+        'is_sub':false,
+        'badges':null
+    },
+    'orginal_text':'#jonny#',
+    'message':[
+        {
+        'type':2,
+        'text':'#jonny#',
+        'asset_list':[{'engine':'Unity','render_type':'SinglePass','asset':{'keyword':'#heart#','version':10,'url':'https://www.google.com/'}}],
+        'animation':{
+            'asset_list':[{'engine':'Unity','render_type':'SinglePass','asset':{'keyword':'wave','version':7,'url':'https://www.google.com/'}}]
+        },
+        'interaction':{}
+        }
+    ],
+    'utc_timestamp':1656761187532
+}
+";
+            // Check all fields
+            var chatSchema = JsonConvert.DeserializeObject<ChatData>(testChatMessage);
+            Assert.NotNull(chatSchema);
+            Assert.AreEqual(chatSchema.chat_id, "8395035a-374b-4272-998e-602c29a8f2af");
+            Assert.AreEqual(chatSchema.utc_timestamp, "1656761187532");
+
+            var author = chatSchema.author;
+            Assert.NotNull(author);
+            Assert.AreEqual(author.platform, "Twitch");
+            Assert.AreEqual(author.name, "tl_tsai");
+            Assert.AreEqual(author.user_id, "120353655");
+            Assert.AreEqual(author.is_mod, false);
+            Assert.AreEqual(author.is_sub, false);
+            Assert.IsNull(author.badges);
+
+            var message = chatSchema.message;
+            Assert.NotNull(message);
+            Assert.AreEqual(message.Count, 1);
+            Assert.AreEqual(message[0].type, (int)ChatMessageType.Emoji_3D);
+            Assert.AreEqual(message[0].text, "#jonny#");
+            Assert.IsNull(message[0].url);
+            Assert.AreEqual(message[0].asset_list[0].engine, "Unity");
+            Assert.AreEqual(message[0].asset_list[0].render_type, "SinglePass");
+            Assert.AreEqual(message[0].asset_list[0].asset.keyword, "#heart#");
+            Assert.NotNull(message[0].animation.asset_list[0]);
+            Assert.AreEqual(message[0].animation.asset_list[0].engine, "Unity");
+            Assert.AreEqual(message[0].animation.asset_list[0].render_type, "SinglePass");
+            Assert.AreEqual(message[0].animation.asset_list[0].asset.keyword, "wave");
+            Assert.NotNull(message[0].interaction);
+        }
+    }
+}
---
+++ Assets/audience-unity-sdk/Core/audience-unity-sdk/Tests/TestEmojiSentencesInAuthor.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using AudienceSDK;
+using NUnit.Framework;
+
+namespace AudienceSDK.UnitTest {
+    public class TestEmojiSentencesInAuthor {
+        private EmojiSentencesInAuthor _emojiSentencesInAuthor;
+
+        [SetUp]
+        public void Setup() {
+            this._emojiSentencesInAuthor = new EmojiSentencesInAuthor(new ChatAuthor());
+        }
+
+        [TearDown]
+        public void TearDown() {
+            this._emojiSentencesInAuthor = null;
+        }
+
+        [Test]
+        public void AddEmojiSentenceButOutOfLimitReturnBufferSizeNotEnough() {
+            UserConfig.EmojiMaxSentencesInOneAuthor = 1;
+
+            var rc1 = this._emojiSentencesInAuthor.AddEmojiSentence(new Queue<EmojiMessage>());
+            var rc2 = this._emojiSentencesInAuthor.AddEmojiSentence(new Queue<EmojiMessage>());
+
+            Assert.AreEqual(rc1, AudienceReturnCode.AudienceSDKOk);
+            Assert.AreEqual(rc2, AudienceReturnCode.AudienceBufferSizeNotEnough);
+        }
+
+        [Test]
+        public void AddEmojiSentenceReturnOk() {
+            var sentence = new Queue<EmojiMessage>();
+            sentence.Enqueue(new Emoji2DMessage());
+            var rc = this._emojiSentencesInAuthor.AddEmojiSentence(sentence);
+
+            Assert.AreEqual(rc, AudienceReturnCode.AudienceSDKOk);
+        }
+
+        [Test]
+        public void AcquireNextEmojiButAuthorIsCoolingDownReturnInvalidState() {
+
+            // Arrange
+            var realtimeSinceStartUp = 0.1f;
+            var timeInterval = 10f;
+
+            var sentence = new Queue<EmojiMessage>();
+            sentence.Enqueue(new Emoji2DMessage());
+            var rc = this._emojiSentencesInAuthor.AddEmojiSentence(sentence);
+
+            // realtimeSinceStartUp - lastAcquireTime(new created author is zero) is smaller than timeinterval,
+            // author is cooling down.
+            // Act
+            EmojiMessage emojiMsg = null;
+            var leftSize = 0;
+            rc = this._emojiSentencesInAuthor.AcquireNextEmoji(realtimeSinceStartUp, timeInterval, out emojiMsg, out leftSize);
+
+            // Assert
+            Assert.IsNull(emojiMsg);
+            Assert.AreEqual(1, leftSize);
+            Assert.AreEqual(AudienceReturnCode.AudienceSDKInvalidState, rc);
+        }
+
+        [Test]
+        public void AcquireNextEmojiReturnMessageAndDequeueEmptyQueue() {
+
+            // Arrange
+            var realtimeSinceStartUp = 0.6f;
+            var timeInterval = 0.5f;
+
+            var sentence1 = new Queue<EmojiMessage>();
+            var sentence2 = new Queue<EmojiMessage>();
+            sentence1.Enqueue(new Emoji2DMessage());
+            sentence2.Enqueue(new Emoji2DMessage());
+
+            var rc = this._emojiSentencesInAuthor.AddEmojiSentence(sentence1);
+            rc = this._emojiSentencesInAuthor.AddEmojiSentence(sentence2);
+
+            // 2 sentence, their count are 1. after acquire emoji, dequeue 1 sentence.
+            // expect sentence count = 1
+            // Act
+            EmojiMessage emojiMsg = null;
+            var leftSize = 0;
+            rc = this._emojiSentencesInAuthor.AcquireNextEmoji(realtimeSinceStartUp, timeInterval, out emojiMsg, out leftSize);
+
+            // Assert
+            Assert.IsNotNull(emojiMsg);
+            Assert.AreEqual(1, leftSize);
+            Assert.AreNotEqual(0f, this._emojiSentencesInAuthor.GetLastAcquireTime());
+            Assert.AreEqual(1, this._emojiSentencesInAuthor.GetSentenceCount());
+        }
+
+        [Test]
+        public void AcquireNextEmojiReturnNullAfterAcquireAllEmojis() {
+
+            // Arrange
+            var realtimeSinceStartUp1 = 0.6f;
+            var realtimeSinceStartUp2 = 1.2f;
+            var timeInterval = 0.5f;
+
+            var sentence = new Queue<EmojiMessage>();
+            sentence.Enqueue(new Emoji2DMessage());
+            var rc = this._emojiSentencesInAuthor.AddEmojiSentence(sentence);
+
+            // Act
+            EmojiMessage emojiMsg1 = null, emojiMsg2 = null;
+            int leftSize1 = 0, leftSize2 = 0;
+            rc = this._emojiSentencesInAuthor.AcquireNextEmoji(realtimeSinceStartUp1, timeInterval, out emojiMsg1, out leftSize1);
+            rc = this._emojiSentencesInAuthor.AcquireNextEmoji(realtimeSinceStartUp2, timeInterval, out emojiMsg2, out leftSize2);
+
+            // Assert
+            Assert.IsNotNull(emojiMsg1);
+            Assert.AreEqual(0, leftSize1);
+            Assert.IsNull(emojiMsg2);
+            Assert.AreEqual(0, leftSize2);
+            Assert.AreNotEqual(0f, this._emojiSentencesInAuthor.GetLastAcquireTime());
+            Assert.AreEqual(0, this._emojiSentencesInAuthor.GetSentenceCount());
+        }
+    }
+}
