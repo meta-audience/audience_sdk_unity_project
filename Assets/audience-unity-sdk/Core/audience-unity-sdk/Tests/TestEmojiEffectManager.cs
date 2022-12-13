e3f7b3f78d2d8f87a53327a3429f3d35ffc197cd
JonnyTsai <Jonny_Tsai@trendmicro.com>
2022-12-13 18:01:25 +0800

    Revert "Revert "Add Test framework""
    This reverts commit b18d416f186f381c1ddeff3df76a57ac1c99fc0d.

---
+++ Assets/audience-unity-sdk/Core/audience-unity-sdk/Tests/TestEmojiEffectManager.cs
@@ -0,0 +1,32 @@
+﻿using AudienceSDK;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace AudienceSDK.UnitTest {
+    class TestEmojiEffectManager {
+
+        private EmojiEffectManager _emojiEffectManager = null;
+
+        [SetUp]
+        public void Setup() {
+            var obj = new GameObject();
+            this._emojiEffectManager = obj.AddComponent<EmojiEffectManager>();
+        }
+
+        [Test]
+        public void TestEmojiAssetListAndUrlNullWillReturnNetworkDBDataError() {
+            ChatAuthor author = new ChatAuthor();
+            EmojiMessage messsage = new Emoji2DMessage();
+            messsage.asset_list = null;
+            messsage.url = null;
+            
+            var rc = this._emojiEffectManager.CreateEmoji(author, messsage);
+
+            LogAssert.Expect(LogType.Error, "[Error][Emoji]CreateEmoji Fail, emoji asset list and url are empty.");
+            Assert.AreEqual(rc, AudienceReturnCode.AudienceSDKNetworkDBDataError);
+        }
+
+    }
+}
