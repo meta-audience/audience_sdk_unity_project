6017c5861cdbaec2ecd8e4168d814f4ee0399bbe
JonnyTsai <Jonny_Tsai@trendmicro.com>
2022-12-15 18:57:35 +0800

    setup sdk

---
+++ Assets/audience-unity-sdk/Editor/AudienceUnitySDKPostProcessor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AudienceUnitySDKPostProcessor : AssetPostprocessor {
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) {
+
+        Debug.Log("Audience OnPostprocessAllAssets");
+        Debug.Log(importedAssets.Length);
+        Debug.Log(deletedAssets.Length);
+        Debug.Log(movedAssets.Length);
+        foreach (string str in importedAssets)
+        {
+            Debug.Log("Reimported Asset: " + str);
+        }
+        foreach (string str in deletedAssets)
+        {
+            Debug.Log("Deleted Asset: " + str);
+        }
+
+        for (int i = 0; i < movedAssets.Length; i++)
+        {
+            Debug.Log("Moved Asset: " + movedAssets[i] + " from: " + movedFromAssetPaths[i]);
+        }
+
+    }
+}
