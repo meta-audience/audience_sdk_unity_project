3b49ec152d3645b391858c3cb6e13bb9b7113061
JonnyTsai <Jonny_Tsai@trendmicro.com>
2022-12-21 18:41:54 +0800

    [DVAT-4751] add sample

---
+++ Assets/audience-unity-sdk/Samples/Scripts/AudiencePanelController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AudienceSDK;
+using UnityEngine;
+
+namespace AudienceSDK.Sample
+{
+    public class AudiencePanelController : MonoBehaviour
+    {
+        private bool audienceControlPanelInited = false;
+
+        private List<NativeSceneSummaryData> CachedProfiles { get; set; }
+
+        private void Start()
+        {
+            if (Audience.Instance == null) {
+                Debug.LogWarning("[AudienceControlPanel] Audience does not exist in this scene.");
+                return;
+            }
+
+            if (Audience.Instance.AudienceInited) {
+                this.InitPanelController();
+            }
+
+            Audience.Instance.onAudienceInitStateChanged += OnAudienceInitStateChanged;
+        }
+
+        private void OnDestroy()
+        {
+            Audience.Instance.onAudienceInitStateChanged -= OnAudienceInitStateChanged;
+        }
+
+        private void InitPanelController() {
+            if (!this.audienceControlPanelInited) {
+                AudienceSDK.Audience.Context.RefreshSceneListCompleted += this.OnRefreshSceneListCompleted;
+                this.audienceControlPanelInited = true;
+            }
+        }
+
+        private void DeInitPanelController() {
+            if (this.audienceControlPanelInited) {
+                AudienceSDK.Audience.Context.RefreshSceneListCompleted -= this.OnRefreshSceneListCompleted;
+                this.audienceControlPanelInited = false;
+            }
+        }
+
+        private void OnAudienceInitStateChanged(bool initState) {
+            if (initState)
+            {
+                this.InitPanelController();
+            }
+            else 
+            {
+                this.DeInitPanelController();
+            }
+        }
+
+        private void OnRefreshSceneListCompleted(List<NativeSceneSummaryData> sceneList)
+        {
+            Debug.LogFormat("[AudienceControlPanel] RefreshSceneListCompleted: size={0}", sceneList.Count);
+            this.CachedProfiles = sceneList;
+        }
+    }
+}
---
+++ Assets/audience-unity-sdk/Samples/Scripts/AudiencePanelView.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudiencePanelView : MonoBehaviour
+{
+    // API button
+    public void Login()
+    {
+
+    }
+
+    public void Logout()
+    {
+    }
+
+    public void RefreshScene()
+    {
+    }
+
+    public void LoadScene()
+    {
+    }
+
+    public void UnloadScene()
+    {
+    }
+
+    public void StartStream()
+    {
+    }
+
+    public void StopStream()
+    {
+    }
+
+    public void UpdateSceneList() {
+
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
