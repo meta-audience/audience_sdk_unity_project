9ea49b1184d2d16874db970b3e712944f16e2ca0
JonnyTsai <Jonny_Tsai@trendmicro.com>
2022-12-22 18:32:48 +0800

    [DVAT-4751] sample

--- Assets/audience-unity-sdk/Samples/Scripts/AudiencePanelView.cs
+++ Assets/audience-unity-sdk/Samples/Scripts/AudiencePanelView.cs
@@ -1,10 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudiencePanelView : MonoBehaviour
 {
-    // API button
+    [SerializeField]
+    private Button minimizePanelButton = null;
+
+    [SerializeField]
+    private Button maxmizePanelButton = null;
+
+    [SerializeField]
+    private GameObject audienceBlocks = null;
+
+    [SerializeField]
+    private GameObject logOnBlock = null;
+
+    [SerializeField]
+    private Button logInButton = null;
+
+    [SerializeField]
+    private Button logOutButton = null;
+
+    [SerializeField]
+    private GameObject sceneBlock = null;
+
+    [SerializeField]
+    private Dropdown sceneListDropdown = null;
+
+    [SerializeField]
+    private Button refreshButton = null;
+
+    [SerializeField]
+    private Button loadButton = null;
+
+    [SerializeField]
+    private Button unloadButton = null;
+
+    [SerializeField]
+    private GameObject streamBlock = null;
+
+    [SerializeField]
+    private Button startStreamButton = null;
+
+    [SerializeField]
+    private Button stopStreamButton = null;
+
+    // UI function
+    public void MaxmizePanel() {
+        this.audienceBlocks.SetActive(true);
+        this.maxmizePanelButton.gameObject.SetActive(false);
+        this.minimizePanelButton.gameObject.SetActive(true);
+    }
+
+    public void MinimizePanel() {
+        this.audienceBlocks.SetActive(false);
+        this.maxmizePanelButton.gameObject.SetActive(true);
+        this.minimizePanelButton.gameObject.SetActive(false);
+    }
+
+    // API function
     public void Login()
     {
 
@@ -38,6 +94,13 @@ public class AudiencePanelView : MonoBehaviour
 
     }
 
+    // UI Control
+    public void ShowLoginCompletedView() {
+        this.sceneBlock.SetActive(true);
+        this.logInButton.interactable = false;
+        this.logOutButton.interactable = true;
+    }
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -47,6 +110,5 @@ public class AudiencePanelView : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        
     }
 }
