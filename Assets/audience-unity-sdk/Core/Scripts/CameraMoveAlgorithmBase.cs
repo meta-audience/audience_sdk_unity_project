045ed0c124f16194c27d190e8c790fe62147cb76
JonnyTsai <Jonny_Tsai@trendmicro.com>
2023-01-19 00:29:34 +0800

    [DVAT-4091] camera algorithm

---
+++ Assets/audience-unity-sdk/Core/Scripts/CameraMoveAlgorithmBase.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AudienceSDK
+{
+    public abstract class CameraMoveAlgorithmBase : MonoBehaviour
+    {
+        private List<Transform> _cameraTransformList = null;
+        private Quaternion _rootRotation = Quaternion.identity;
+        private Vector3 _rootPosition = Vector3.zero;
+
+        protected void MoveCameras(Quaternion rotation, Vector3 position) {
+            foreach (var cameraTransform in this._cameraTransformList) {
+                if (cameraTransform)
+                {
+                    var localRotation = Quaternion.Inverse(this._rootRotation) * cameraTransform.rotation;
+                    var localPosition = Quaternion.Inverse(this._rootRotation) * (cameraTransform.position - this._rootPosition);
+                    this._rootRotation = rotation;
+                    this._rootPosition = position;
+                    cameraTransform.rotation = this._rootRotation * localRotation;
+                    cameraTransform.position = this._rootPosition + this._rootRotation * localPosition;
+                }
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            this._cameraTransformList = new List<Transform>();
+        }
+
+        protected virtual void Start()
+        {
+            if (Audience.AudienceInited)
+            {
+                this.RegisterCameraDirectorCallback(true);
+            }
+
+            Audience.AudienceInitStateChanged += this.RegisterCameraDirectorCallback;
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (Audience.AudienceInited)
+            {
+                this.RegisterCameraDirectorCallback(false);
+            }
+
+            Audience.AudienceInitStateChanged -= this.RegisterCameraDirectorCallback;
+        }
+
+        private void RegisterCameraDirectorCallback(bool audienceInit)
+        {
+            if (audienceInit)
+            {
+                Audience.Context.SceneManager.CurrentSceneManagerStateChanged += this.OnSceneManagerStateChanged;
+                if (Audience.Context.SceneManager.CurrentSceneManagerState == SceneManager.SceneManagerState.Loaded)
+                {
+                    this.CollectCameras();
+                }
+            }
+            else
+            {
+                this._cameraTransformList.Clear();
+                Audience.Context.SceneManager.CurrentSceneManagerStateChanged -= this.OnSceneManagerStateChanged;
+            }
+        }
+
+        private void OnSceneManagerStateChanged(SceneManager.SceneManagerState state)
+        {
+            switch (state)
+            {
+                case SceneManager.SceneManagerState.Loaded:
+                    {
+                        this.CollectCameras();
+                        break;
+                    }
+                case SceneManager.SceneManagerState.Unload:
+                    {
+                        this._cameraTransformList.Clear();
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+
+        // collect camera's transform.
+        private void CollectCameras()
+        {
+            var audienceCameras = Resources.FindObjectsOfTypeAll<AudienceCameraBehaviourBase>();
+            foreach (var audienceCamera in audienceCameras)
+            {
+                this._cameraTransformList.Add(audienceCamera.transform);
+            }
+        }
+
+    }
+}
---
+++ Assets/audience-unity-sdk/Samples/Scripts/DefaultDockCameraAlgorithm.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AudienceSDK.Sample
+{
+    public class DefaultDockCameraAlgorithm : CameraMoveAlgorithmBase
+    {
+        [SerializeField]
+        public Transform _dockTarget = null;
+
+        protected override void Start() {
+            base.Start();
+            if (this._dockTarget)
+            {
+                this.MoveCameras(this._dockTarget.rotation, this._dockTarget.position);
+            }
+        }
+
+        private void Update()
+        {
+            if (this._dockTarget)
+            {
+                this.MoveCameras(this._dockTarget.rotation, this._dockTarget.position);
+            }
+        }
+    }
+}
